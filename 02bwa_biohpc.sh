6286aeb86ab6a4a9b60333da53687b309f6eff8a
JamieCFreeman <30966718+JamieCFreeman@users.noreply.github.com>
2019-11-26 13:52:55 -0500

    add PE, variable check

--- 02bwa_biohpc.sh
+++ 02bwa_biohpc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# edit 2019-11-26
+
 # General command for using bwa mem to map short reads to a reference genome
 # Must be run in parent directory that has subfolders: 
 # 		trim_fastq, including fastq.gz files for mapping
@@ -14,16 +16,37 @@
 # REF="/workdir/jcf236/bwa_dir/mds_ref.fa.gz", reference fasta as .fa or .fa.gz
 # CORES to set # threads, bwa uses as many as you give it
 # ID is sample name prefix for file
+# PE is yes/no: yes when data is pair end, no when single 
 
+# Check that variables are set & exit otherwise
+if [ -z "$RG_string" ] || [ -z "$REF" ] || [ -z "$CORES ] || [ -z "$ID" ] || [ -z "$PE" ];
+then
+ echo "One or more variables are undefined."
+ exit 1
+fi
 
+# Single-end command:
+if [ "$PE" = "no" ]; 
+	then
+	# Pipe bwa mem output directly to samtools to make bam (sam bigger)
+	bwa mem -M -R ${RG_string} -t ${CORES} ${REF} ./trim_fastq/${ID}_trim.fastq.gz 2> ./bam/${ID}_bwa.log | \
+		samtools view -bS - > ./bam/${ID}.bam
+		samtools flagstat ./bam/${ID}.bam > ./bam/${ID}.stats
+		samtools sort -@ ${CORES} ./bam/${ID}.bam > ./sort_bam/${ID}_sort.bam
+		samtools index ./sort_bam/${ID}_sort.bam
+		samtools flagstat ./sort_bam/${ID}_sort.bam > ./sort_bam/${ID}_sort.stats
+	fi
 
-# Pipe bwa mem output directly to samtools to make bam (sam bigger)
-bwa mem -M -R ${RG_string} -t ${CORES} ${REF} ./trim_fastq/${ID}_trim.fastq.gz 2> ./bam/${ID}_bwa.log | \
-	samtools view -bS - > ./bam/${ID}.bam
-	samtools flagstat ./bam/${ID}.bam > ./bam/${ID}.stats
-	samtools sort -@ ${CORES} ./bam/${ID}.bam > ./sort_bam/${ID}_sort.bam
-	samtools index ./sort_bam/${ID}_sort.bam
-	samtools flagstat ./sort_bam/${ID}_sort.bam > ./sort_bam/${ID}_sort.stats
+# Paired-end command
+if [ "$PE" = "yes" ]; 
+	then
+	# Pipe bwa mem output directly to samtools to make bam (sam bigger)
+	bwa mem -M -R ${RG_string} -t ${CORES} ${REF} ./trim_fastq/${ID}_trim.fastq.gz 2> ./bam/${ID}_bwa.log | \
+		samtools view -bS - > ./bam/${ID}.bam
+		samtools flagstat ./bam/${ID}.bam > ./bam/${ID}.stats
+		samtools sort -@ ${CORES} ./bam/${ID}.bam > ./sort_bam/${ID}_sort.bam
+		samtools index ./sort_bam/${ID}_sort.bam
+		samtools flagstat ./sort_bam/${ID}_sort.bam > ./sort_bam/${ID}_sort.stats
 
 # Compare sorted stats file to unsorted stats file. Should be the same- # of reads will only change if one is truncated.
 # If they are the same, we want to delete the unsorted.
